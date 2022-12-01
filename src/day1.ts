9120aa9504540e3bec7ba21314c2f4cdc1ec5274
Leo <leo.maedje@gmail.com>
2022-12-01 13:36:07 +0000

    Set up repo readme and first day  for advent of code

---
+++ src/day1.ts
@@ -0,0 +1,10 @@
+
+/**
+ * Day 1 of Advent of Code 2022
+ * @param allFood A list of the calories of each food item carried by each elf, with each elf's inventory separated by an empty line
+ * @returns The highest number of total calories carried by any elf
+ */
+export function findElfWithMostCalories(allFood: string): number {
+
+    return 0;
+}
