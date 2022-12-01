8ccced604739a73d7c436ff14ba65df3fec88c57
Leo <leo.maedje@gmail.com>
2022-12-01 14:34:09 +0000

    Wrote tests and functions for part 2 of day 1, tests pass!

--- src/day1.test.ts
+++ src/day1.test.ts
@@ -1,4 +1,4 @@
-import { findElfWithMostCalories, sumCalories } from "./day1";
+import { findElfWithMostCalories, findTopThreeElvesWithMostCalories, sumCalories } from "./day1";
 
 test("findElfWithHighestCalories identifies the correct max calories carried by any elf", () => {
   const noSum: string = `1\n\n2\n\n3`;
@@ -18,3 +18,12 @@ test("sumCalories returns the correct sum of calories", () => {
   const inputCals3: string = `123239\n234243\n5902\n3424`;
   expect(sumCalories(inputCals3)).toBe(366808);
 });
+
+
+test("findTopThreeElvesWithMostCalories returns the sum of the total calories carried by the three elves with the most calories", () => {
+  const testFood1: string = `1\n\n2\n\n3\n\n4\n\n5\n\n6`;
+  expect(findTopThreeElvesWithMostCalories(testFood1)).toBe(15);
+
+  const testFood2: string = `1\n1000\n\n2\n1000\n\n3\n1000\n\n4\n\n5\n\n6`;
+  expect(findTopThreeElvesWithMostCalories(testFood2)).toBe(3006);
+})
--- src/day1.ts
+++ src/day1.ts
@@ -1,7 +1,7 @@
 import { day1Data } from "./day1-data";
 
 /**
- * Day 1 of Advent of Code 2022
+ * Day 1, part 1 of Advent of Code 2022
  * @param allFood A list of the calories of each food item carried by each elf, with each elf's inventory separated by an empty line
  * @returns The highest number of total calories carried by any elf
  */
@@ -26,4 +26,51 @@ export function sumCalories(foodItemsOfOneElf: string): number {
 }
 
 const day1Result: number = findElfWithMostCalories(day1Data);
-console.log("The elf carrying the most calories and the solution to day 1 is: ", day1Result);
+console.log("The elf carrying the most calories and the solution to day 1, part 1, is: ", day1Result);
+
+/**
+ * Day 1, part 2 of Advent of Code 2022
+ * @param allFood A list of the calories of each food item carried by each elf, with each elf's inventory separated by an empty line
+ * @returns The number of total calories carried by the three elves carrying the most calories
+ */
+export function findTopThreeElvesWithMostCalories(allFood: string): number {
+    const foodItemsByElf: string[] = allFood.split("\n\n");
+    const caloriesByElf: number[] = foodItemsByElf.map(sumCalories);
+    const sortedCalories: number[] = radixSortCalories(caloriesByElf);
+    const topThreeCaloriesByElf: number[] = sortedCalories.slice(-3);
+    const totalTopThreeCalories: number = topThreeCaloriesByElf.reduce((nextCalories, currentCalories) => nextCalories + currentCalories, 0);
+    return totalTopThreeCalories;
+}
+
+export function radixSortCalories(caloriesByElf: number[]): number[] {
+    let allCalories: number[] = [...caloriesByElf]
+    const maxDigits: number = mostDigits(allCalories);
+    for (let digitPlace = 0; digitPlace < maxDigits; digitPlace++) {
+        const digitBuckets: number[][] = Array.from({length: 10}, () => []);
+        for (const elfCalories of allCalories) {
+            const digit = getDigit(elfCalories, digitPlace);
+            digitBuckets[digit].push(elfCalories);
+        }  
+        allCalories = [0].concat(...digitBuckets);
+    }
+    return allCalories;
+}
+
+function mostDigits(allCalories: number[]): number {
+    let maxDigits = 0;
+    for (const elfCalories of allCalories) {
+        maxDigits = Math.max(maxDigits, digitCount(elfCalories));
+    }
+    return maxDigits;
+}
+
+function digitCount(elfCalories: number): number {
+    if (elfCalories === 0) return 1;
+    return Math.floor(Math.log10(elfCalories)) + 1;
+}
+
+function getDigit(elfCalories: number, digitPlaceFromBack: number): number {
+    const allDigitsUpToDigit: number = Math.floor(elfCalories/Math.pow(10, digitPlaceFromBack));
+    const lastRemainingDigit: number = allDigitsUpToDigit % 10;
+    return lastRemainingDigit;
+}
