c812be52c7ca78d8022dc602787cbc670dda1e0a
Leo <leo.maedje@gmail.com>
2022-12-06 11:07:18 +0000

    Made minor changes to day 6 part 1 to fit day 6 part 2 as well, solution is correct

--- src/day6/day6.test.ts
+++ src/day6/day6.test.ts
@@ -1,8 +1,16 @@
 import { findFirstMarker } from "./day6";
 
-test("test for findFirstMarker", () => {
-  expect(findFirstMarker("bvwbjplbgvbhsrlpgdmjqwftvncz")).toBe(5);
-  expect(findFirstMarker("nppdvjthqldpwncqszvftbrmjlhg")).toBe(6);
-  expect(findFirstMarker("nznrnfrfntjfmvfwmzdfjlvtqnbhcprsg")).toBe(10);
-  expect(findFirstMarker("zcfzfwzzqfrljwzlrfnpqdbhtmscgvjw")).toBe(11);
+test("test for findFirstMarker for part 1", () => {
+  expect(findFirstMarker("bvwbjplbgvbhsrlpgdmjqwftvncz", 4)).toBe(5);
+  expect(findFirstMarker("nppdvjthqldpwncqszvftbrmjlhg", 4)).toBe(6);
+  expect(findFirstMarker("nznrnfrfntjfmvfwmzdfjlvtqnbhcprsg", 4)).toBe(10);
+  expect(findFirstMarker("zcfzfwzzqfrljwzlrfnpqdbhtmscgvjw", 4)).toBe(11);
+});
+
+test("test for findFirstMarker for part 2", () => {
+  expect(findFirstMarker("mjqjpqmgbljsphdztnvjfqwrcgsmlb", 14)).toBe(19);
+  expect(findFirstMarker("bvwbjplbgvbhsrlpgdmjqwftvncz", 14)).toBe(23);
+  expect(findFirstMarker("nppdvjthqldpwncqszvftbrmjlhg", 14)).toBe(23);
+  expect(findFirstMarker("nznrnfrfntjfmvfwmzdfjlvtqnbhcprsg", 14)).toBe(29);
+  expect(findFirstMarker("zcfzfwzzqfrljwzlrfnpqdbhtmscgvjw", 14)).toBe(26);
 });
--- src/day6/day6.ts
+++ src/day6/day6.ts
@@ -1,11 +1,11 @@
 import { dataStreamBuffer } from "./day6-data";
 
 /**
- * Day 6, part 1 of Advent of Code 2022
+ * Day 6, parts 1 and 2 of Advent of Code 2022
  * @param buffer string of characters
  * @returns the ordinal value of the character after which four subsequent distinct characters have occurred
  */
-export function findFirstMarker(buffer: string): number {
+export function findFirstMarker(buffer: string, markerLength: number): number {
   let marker: string = "";
   for (let i = 0; i < buffer.length; i++) {
     const char: string = buffer[i];
@@ -14,15 +14,21 @@ export function findFirstMarker(buffer: string): number {
       marker = marker.slice(indexOfEarlierOccurrence + 1);
     }
     marker += char;
-    if (marker.length === 4) {
+    if (marker.length === markerLength) {
       return i + 1;
     }
   }
   return Infinity;
 }
 
-const day6Part1Result: number = findFirstMarker(dataStreamBuffer);
+const day6Part1Result: number = findFirstMarker(dataStreamBuffer, 4);
 console.log(
-  "Day6, Part 1: The first marker is complete after character: ",
+  "Day6, Part 1: The first start-of-packet marker is complete after character: ",
   day6Part1Result,
+);
+
+const day6Part2Result: number = findFirstMarker(dataStreamBuffer, 14);
+console.log(
+  "Day6, Part 2: The first start-of-message marker is complete after character: ",
+  day6Part2Result,
 );
