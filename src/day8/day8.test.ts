ebd2e95b74fe086c2210f756f62a1cea21875a15
Leo <leo.maedje@gmail.com>
2022-12-08 12:28:41 +0000

    Wrote function and test for day 8 part 2, solution correct

--- src/day8/day8.test.ts
+++ src/day8/day8.test.ts
@@ -1,4 +1,4 @@
-import { countVisibleTrees } from "./day8";
+import { countVisibleTrees, findHighestScenicScore } from "./day8";
 
 test("test for countVisibleTrees", () => {
   const testInput: string = `30373
@@ -7,4 +7,13 @@ test("test for countVisibleTrees", () => {
 33549
 35390`;
   expect(countVisibleTrees(testInput)).toBe(21);
+});
+
+test("test for findHighestScenicScore", () => {
+  const testInput: string = `30373
+25512
+65332
+33549
+35390`;
+  expect(findHighestScenicScore(testInput)).toBe(8);
 });
--- src/day8/day8.ts
+++ src/day8/day8.ts
@@ -110,3 +110,65 @@ interface Visibilities {
 
 const day8Part1Result: number = countVisibleTrees(bigForest);
 console.log("Day 8, Part 1: The number of visible trees is: ", day8Part1Result);
+
+/**
+ Day 8, Part 2 of Advent of Code 2022
+ * @param forest string of tree heighs - string
+ * @returns highest scenic score - number
+ */
+export function findHighestScenicScore(forest: string): number {
+  const forestGrid: number[][] = forest
+    .split("\n")
+    .map((forestRow) =>
+      forestRow.split("").map((treeString) => parseInt(treeString)),
+    );
+  let highScore: number = 0;
+  for (let i = 1; i < forestGrid.length - 1; i++) {
+    for (let j = 1; j < forestGrid[1].length - 1; j++) {
+      const currentScore: number = calculateScore(forestGrid, i, j);
+      highScore = Math.max(currentScore, highScore);
+    }
+  }
+  return highScore;
+}
+
+function calculateScore(grid: number[][], row: number, col: number): number {
+  const tree: number = grid[row][col];
+  let [left, right, top, down]: number[] = [0, 0, 0, 0];
+  for (let i = col - 1; i >= 0; i--) {
+    const currentTree: number = grid[row][i];
+    if (currentTree >= tree) {
+      left++;
+      break;
+    }
+    left++;
+  }
+  for (let i = col + 1; i < grid[0].length; i++) {
+    const currentTree: number = grid[row][i];
+    if (currentTree >= tree) {
+      right++;
+      break;
+    }
+    right++;
+  }
+  for (let i = row - 1; i >= 0; i--) {
+    const currentTree: number = grid[i][col];
+    if (currentTree >= tree) {
+      top++;
+      break;
+    }
+    top++;
+  }
+  for (let i = row + 1; i < grid.length; i++) {
+    const currentTree: number = grid[i][col];
+    if (currentTree >= tree) {
+      down++;
+      break;
+    }
+    down++;
+  }
+  return left * right * top * down;
+}
+
+const day8Part2Result: number = findHighestScenicScore(bigForest);
+console.log("Day 8, Part 2: The highest scenic score is: ", day8Part2Result);
